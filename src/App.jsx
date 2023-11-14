5ca8285f2bddee14a452313a64909aecfc4debfd
jsanchez22 <sanchez.franco22@gmail.com>
2023-11-14 07:56:16 -0300

    tesis

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ItemList } from "./components/ItemList";
+
+import { Navbar } from "./components/Navbar";
+import {Contact} from "./components/Contact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ShoppingCart } from "./components/ShoppingCart";
+import { ShoppingCartProvider } from "./contexts/ShoppingCartContext";
+
+export const App = () => {
+  return (
+    <ShoppingCartProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ItemList />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </ShoppingCartProvider>
+  );
+};
---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Contact = () => {
+    return (
+      <div className="items-list">
+        <h1 className='nada'>contacto</h1>
+      </div>
+    );
+  };
+  
