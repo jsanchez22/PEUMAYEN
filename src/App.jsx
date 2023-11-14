0c0425101ecbd1c5af68a83c075da0bc2ac401de
jsanchez22 <sanchez.franco22@gmail.com>
2023-11-14 20:53:24 -0300

    avanzado, falta el footer2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { ItemList } from "./components/ItemList";
-
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import {Contact} from "./components/Contact";
